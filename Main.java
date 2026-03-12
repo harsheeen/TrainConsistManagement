288a38ff40f82d68698450b5368e61454cdf7516
harsheeen <harsheenkaur023@gmail.com>
2026-03-12 16:55:32 +0530

    Add: Track Unique Bogie IDs (Set – HashSet)

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
-package com.TrainManagement.UseCaseTwo;
-import java.util.ArrayList;
-import java.util.List;
+package com.TrainManagement.UseCaseThree;
+import java.util.*;
 
 /**
  * 
- * USECASE 2 - TRAIN CONSIST MANAGEMENT
+ * USECASE 3 - TRAIN CONSIST MANAGEMENT
  * INITIALISE TRAIN AND USE FUNCTIONS SUCH AS add, remove, contains
  * DISPLAY CONSIST SUMMARY
  * 
@@ -16,46 +15,42 @@ import java.util.List;
  * - USES FUNCTIONS SUCH AS ADD, REMOVE, CONTAINS
  * 
  * @author Harsheen
- * @version 2.0
+ * @version 3.0
  */
 public class Main {
 	public static void main(String args[]) {
 
 		System.out.println("============================================");
 		System.out.println("======TRAIN CONSIST MANAGEMENT SYSTEM ======");
-		System.out.println("====================UC-2===================");
+		System.out.println("====================UC-3===================");
 
-		//initialize the arraylist
-		List<String> trainConsist=new ArrayList<>();
+		Set<String> trainConsist=new HashSet<String>();
 
 		System.out.println("Train initialised successfully");
 		//.size() returns the initial size - number of element sin arraylist
 		System.out.println("Initial Bogie Count:  "+trainConsist.size());
-
-		//add elements to the arraylist
-		trainConsist.add("AC");
-		trainConsist.add("Sleeper");
-		trainConsist.add("AC Chair");
-		trainConsist.add("First Class");
+		
+		//add(ele) to the set
+		trainConsist.add("B101");
+		trainConsist.add("B102");
+		trainConsist.add("B103");
+		trainConsist.add("B104");
 		System.out.println("Current Train Consist:  "+trainConsist);
 
-		//remove AC using function.remove(ele)
-		trainConsist.remove("AC");
-		System.out.println("Current Train Consist (trainConsist.remove(AC)): "+trainConsist);
-		
-		//remove index 0 using remove(index)
-		trainConsist.remove(0);
-		System.out.println("Current Train Consist (trainConsist.remove(0)):  "+trainConsist);
+		//remove(ele) from the set
+		trainConsist.remove("B102");
+		System.out.println("Current Train Consist (trainConsist.remove(B102)): "+trainConsist);
 
-		//add(index,ele)
-		trainConsist.add(1,"Second Class");
-		System.out.println("Current Train Consist (trainConsist.add(1, Second Class): "+trainConsist);
+		trainConsist.add("B101");
+		System.out.println("Current Train Consist after adding duplicates (trainConsist.add(B101)): "+trainConsist);
+
 
 		boolean contains=true;
-		//contains return boolean value on the basis if element exist 
-		contains=trainConsist.contains("AC Chair");
 
-		System.out.println("Is AC present ? :  "+contains);
+		contains=trainConsist.contains("B104");
 
-	}
+		System.out.println("Is B104 present ? :  "+contains);
+
+	}	
+
 }
