50e3b371b6bed405b5a8b5fd4a96787eefcb25eb
harsheeen <harsheenkaur023@gmail.com>
2026-03-12 22:55:07 +0530

    Add: Sort Bogie Names Using Arrays.sort()

--- Main.java
+++ Main.java
@@ -1,52 +1,44 @@
-package com.TrainManagement.UseCaseSixteen;
+package com.TrainManagement.UseCaseSeventeen;
 /**
  * 
- * USECASE 16 - TRAIN CONSIST MANAGEMENT
- * Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)
+ * USECASE 17 - TRAIN CONSIST MANAGEMENT
+ * Sort Bogie Names Using Arrays.sort()
  * 
  * DESCRIPTION :
- * - CREATES AN ARRAY OF CAPACITY
- * - COMPARES ADJACENT VALUES
- * - SWAPS VALUES WHEN REQUIRED
- * - REPEATS UNTIL SORTED
- * - DISPLAY SORTED RESULT
+ * 
+ * - CREATES AN ARRAY OF BOGGIE NAMES
+ * - USES ARRAY.SORT() FOR SORTING
+ * - DISPLAYS THE SORTED RESULTS
  * 
  * @author Harsheen
- * @version 16.0
+ * @version 17.0
  */
+import java.util.Arrays;
+
 public class Main {
 	//Helper function-to print the elements of array
-	public static void print(int arr[]) {
+	public static void print(String arr[]) {
 		for(int i=0;i<arr.length;i++)
-			System.out.print(arr[i]+" ");
+			System.out.print(" [ " + arr[i]+" ] ");
 		System.out.println();
 	}
 	
 	public static void main(String args[]) {
 		System.out.println("============================================");
 		System.out.println("======TRAIN CONSIST MANAGEMENT SYSTEM ======");
-		System.out.println("==================UC-16=====================");
-
-		//initialize the array
-		int[] capacity= {10,23,11,9,8,43};
-		System.out.println("Original Array");
-		print(capacity);
+		System.out.println("==================UC-17=====================");
 		
-		//Bubble sort logic
-		//outer loop
-		for(int i=0;i<capacity.length;i++) {
-			//inner loop
-			for(int j=0;j<capacity.length-i-1;j++) {
-
-				if(capacity[j]>capacity[j+1]) {
-					int temp=capacity[j];
-					capacity[j]=capacity[j+1];
-					capacity[j+1]=temp;
-				}
-			}
-
-		}
-		System.out.println("Array after applying bubble sort");
-		print(capacity);
+		//Initialize the Array
+		String[] Boogie_names= {"Sleeper", "AC Chair", "Luxury", "General","First Class"};
+		System.out.println("Before Sorting");
+		
+		//printing the elements in their insertion order
+		print(Boogie_names);
+		System.out.println();
+		
+		//Arrays.sort()- sort the array of strings in lexicographically order
+		Arrays.sort(Boogie_names);
+		System.out.println("After Sorting");
+		print(Boogie_names);
 	}
-}
+	}
