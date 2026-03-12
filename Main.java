b5d67bdda76d77164c398ac08c46c904350d5973
harsheeen <harsheenkaur023@gmail.com>
2026-03-12 22:58:05 +0530

    Add: Binary Search for Bogie ID (Optimized Searching)

--- Main.java
+++ Main.java
@@ -1,51 +1,73 @@
-package com.TrainManagement.UseCaseEighteen;
+package com.TrainManagement.UseCaseNineteen;
 
 import java.util.Arrays;
 /**
  * 
- * USECASE 18 - TRAIN CONSIST MANAGEMENT
- * Linear Search for Bogie ID (Array-Based Searching)
+ * USECASE 19 - TRAIN CONSIST MANAGEMENT
+ * Binary Search for Bogie ID (Optimized Searching)
  * 
  * DESCRIPTION :
- * 
- * - CREATES AN ARRAY OF BOGGIE IDs
- * - ACEPTS A SEARCH KEY
- * - TRAVERSETHE ARRAY SEQUENTIALLY
- * - STOPS WHEN ARRAY IS FOUND
- * - DISPLAYS THE SEARCH RESULT
+ * - CREATES SORTED BOGGIE ID ARRAY
+ * - DEFINES SEARCH KEY
+ * - APPLIES BINARY SEARCH LOGIC
+ * - NARROWS THE SEARCH RANGE ITERATION
  * 
  * @author Harsheen
- * @version 18.0
+ * @version 19.0
  */
 public class Main {
+	//
+	public static void print(String arr[]) {
+		for(int i=0;i<arr.length;i++)
+			System.out.print(" [ " + arr[i]+" ] ");
+		System.out.println();
+	}
 	public static void main(String args[]) {
 		System.out.println("============================================");
 		System.out.println("======TRAIN CONSIST MANAGEMENT SYSTEM ======");
-		System.out.println("==================UC-20=====================");
-
-		//Initialize an array of Strings
-		String[] boogie_id= {"B101","B102","B103","B104"};
-		
-		//Search_id defines the train to be searched
-		String Search_id="B103";
-		System.out.println("Present Boogie IDs are");
+		System.out.println("====================UC-19===================");
 		
 		
-		Arrays.stream(boogie_id).forEach(x -> System.out.print(x + " "));
+		//Initialize the array of strings
+		String[] Boogie_names= {"Sleeper", "AC Chair", "Luxury", "General","First Class"};
+		String Search_id="Sleeper";
+		
+		//Print the array before sorting
+		System.out.println("Before Sorting");
+		print(Boogie_names);
 		System.out.println();
-		boolean find=false;
 		
-		//for-each loop to find the Search_id element 
-		for(String boogie:boogie_id) {
-			if(boogie.equals(Search_id)) {
+		//Sort the array
+		Arrays.sort(Boogie_names);
+		System.out.println("After Sorting");
+		print(Boogie_names);
+		
+		
+		boolean find=false;
+		int low=0,mid;
+		int high=Boogie_names.length-1;
+		
+		//Apply Binary Search logic on the sorted array
+		while(low<=high) {
+			mid = low + (high - low) / 2;
+			if(Boogie_names[mid].equals(Search_id)) {  
 				find=true;
 				break;
 			}
+			else if(Boogie_names[mid].compareTo(Search_id)<0) {  //if the element comes after the mid element, shift low to mid+1
+				low=mid+1;
+			}
+			else {
+				high=mid-1;   //if the element comes before the mid element, shift high to mid-1
+			}
+
 		}
-		System.out.print("Is " +Search_id+" Present?");
-		System.out.print(find ? " Yes " : "No");
+		System.out.println();
+		System.out.print("Can you find "+Search_id+" : ");
+		System.out.println((find)?"Found":"Not Found");
+
 
 
 	}
-
 }
+
